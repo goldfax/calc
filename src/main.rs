64c79b204ec36494f239d32f34b31b326f7c40fa
Nathaniel <nathanield.freed22@gmail.com>
2018-12-06 20:56:16 -0800

    Pregenerated files

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+
+use relm::{Relm, Update, Widget};
+use gtk::prelude::*;
+use gtk::{Window, Inhibit, WindowType};
+
+struct Model{
+    x: u32
+}
+
+fn main(){
+    
+}
